f62f02b232d1f64df994a21d0bc442645c1c3336
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-12-02 11:16:36 +0100

    add fonction addMessage

--- src/app/containers/ChatComplete/ChatComplete.jsx
+++ src/app/containers/ChatComplete/ChatComplete.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import ChatWindow from '../chatWindow/ChatWindow.jsx';
 import ChatInput from '../chatInput/ChatInput.jsx';
@@ -6,13 +6,22 @@ import ChatInput from '../chatInput/ChatInput.jsx';
 import './chatComplete.scss';
 
 export default class ChatComplete extends Component {
+
+  /**
+   *
+   * @param message
+   */
+  addMessage(message) {
+    this.props.addMessage(message);
+  }
+
   render() {
     return (
       <section className="chat">
         <ChatWindow messages={this.props.messages}
                     user={this.props.user}/>
         <p className="user-name">{this.props.user}</p>
-        <ChatInput addMessage={(message)=>this.props.addMessage(message)}
+        <ChatInput addMessage={message => this.addMessage(message)}
                    user={this.props.user}/>
       </section>
     )
