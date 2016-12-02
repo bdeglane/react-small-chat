0dc8436ac4d40c70b9ef1590ebbe68c3316e5595
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-12-02 13:45:59 +0100

    component now stateless

--- src/app/pages/Chat.jsx
+++ src/app/pages/Chat.jsx
@@ -1,34 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import ChatComplete from '../containers/ChatComplete/ChatComplete.jsx';
 
 import './chat.scss';
 
 export default class Chat extends Component {
-  constructor() {
-    super();
-    this.state = {
-      messages: []
-    };
-  }
-
-  /**
-   *
-   * @param message
-   */
-  addMessage(message) {
-    this.setState(
-      {messages: [...this.state.messages, message]},
-      // log in console new message and new state
-      ()=> {
-        console.group('new message');
-        console.info('from', message.user, 'on', message.date);
-        console.info('new state ', this.state);
-        console.groupEnd('new message');
-      }
-    );
-  }
-
   render() {
     return (
       <div>
@@ -36,12 +12,12 @@ export default class Chat extends Component {
           <h1>Small Chat</h1>
         </header>
         <div className="chat-app">
-          <ChatComplete messages={this.state.messages}
-                        addMessage={(message)=>this.addMessage(message)}
+          <ChatComplete messages={this.props.messages}
+                        addMessage={(message) => this.props.addMessage(message)}
                         user={'Roger'}
           />
-          <ChatComplete messages={this.state.messages}
-                        addMessage={(message)=>this.addMessage(message)}
+          <ChatComplete messages={this.props.messages}
+                        addMessage={(message) => this.props.addMessage(message)}
                         user={'Julie'}
           />
         </div>
